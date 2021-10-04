666643ffdbcd7d4f8a8618ee40ebe6f6bd6f397e
leaburriat <leaburriat@localhost>
2021-10-04 17:40:18 +0200

    creation du depot MANIP NB INT

---
+++ manipNombreInt_KERVELLA_BURRIAT/src/manipnombreint_kervella_burriat/ManipNombreInt_KERVELLA_BURRIAT.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package manipNombreInt;
+import java.util.Scanner;
+/**
+ *
+ * @author leaburriat
+ */
+public class manipNombresInt_KERVELLA_BURRIAT {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int nb1;
+        int nb2;
+        int add;
+        int diff;
+        int prod;
+        int quot;
+        int reste;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Entrez deux valeurs");
+        nb1=sc.nextInt();
+        nb2=sc.nextInt();
+        add=nb1+nb2;
+        diff=nb1-nb2;
+        prod=nb1*nb2;
+        System.out.println("Résultat de la somme est : "+add+ "\nRésultat de la différence est : "+ diff+ "\nRésultat du produit : "+prod);
+        quot = nb1 / nb2; // division entier
+        reste = nb1 % nb2; // reste de la division euclidienne
+        System.out.println("Résultat de la division entière : "+quot+"\nReste de la division euclidienne : "+reste);
+    }
+    
+}
+
