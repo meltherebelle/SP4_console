678c1eb0da077b0219aa926d41f795775762a444
leaburriat <leaburriat@localhost>
2021-10-04 17:13:43 +0200

    creation du depot

---
+++ TP1/src/stats_KERVELLA_BURRIAT.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author leaburriat
+ */
+public class stats_KERVELLA_BURRIAT {
+    
+    public static void main(String[] args) {
+        int [] Face = new double[6];
+        for (int i = 0; i < 5; i++) {
+            Face[i]=i+1;
+        System.out.println(Face[i]);
+        }
+    }
+}
