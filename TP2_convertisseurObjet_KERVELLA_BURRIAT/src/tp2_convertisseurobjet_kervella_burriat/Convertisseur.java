1b3fbe5e6dd97af0d49e9612a3e03286017bbcbf
leaburriat <leaburriat@localhost>
2021-11-05 16:23:19 +0100

    re-création Convertisseur (enlever beugs Rename)

--- TP2_convertisseurObjet_KERVELLA_BURRIAT/src/tp2_convertisseurobjet_kervella_burriat/Convertisseur.java
+++ TP2_convertisseurObjet_KERVELLA_BURRIAT/src/tp2_convertisseurobjet_kervella_burriat/Convertisseur.java
@@ -1,59 +1,56 @@
+package tp2_convertisseurobjet_kervella_burriat;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package tp2_convertisseurobjet_kervella_burriat;
 
 /**
  *
  * @author leaburriat
  */
-class Convertisseur {
-
-    public Convertisseur() {
-        int nbConversions = 0;
-        // incrémenter nbConversions de 1 à chaque conversion
-    }
+public class Convertisseur {
+    int nbConversions = 0; // incrémenter nbConversions de 1 à chaque conversion
 
     // La méthode toString() doit retourner le nombre de conversions effectuées
     @Override
-    public String toString() {
+        public String toString() {
         return "nb de conversions" + nbConversions;
     }
 
-    public void CelsiusVersKelvin(float tempC) {
+    public float CelsiusVersKelvin(float tempC) {
         float tempK;
-        tempK = tempC + 273.15; // K = °C + 273,15.
+        tempK = (float) (tempC + 273.15); // K = °C + 273,15.
         return tempK;
     }
 
-    public void KelvinVersCelcius(float tempK) {
+    public float KelvinVersCelcius(float tempK) {
         float tempC;
-        tempC = tempK - 273.15; // °C = K - 273,15.
+        tempC = (float) (tempK - 273.15); // °C = K - 273,15.
         return tempC;
     }
 
-    public void FarenheitVersCelcius(float tempF) {
+    public float FarenheitVersCelcius(float tempF) {
         float tempC;
         tempC = (tempF - 32) * (5 / 9); // (°F − 32) × 5/9 = °C
         return tempC;
     }
 
-    public void CelciusVersFarenheit(float tempC) {
+    public float CelciusVersFarenheit(float tempC) {
         float tempF;
         tempF = (tempC * (9 / 5)) + 32; // (0°C × 9/5) + 32 = 32°F
         return tempF;
     }
 
-    public void KelvinVersFarenheit(float tempK) {
+    public float KelvinVersFarenheit(float tempK) {
         float tempF;
-        tempF = (tempK - 273.15) * (9 / 5) + 32; // (0K − 273.15) × 9/5 + 32 = -459.7°F
+        tempF = (float) ((tempK - 273.15) * (9 / 5) + 32); // (0K − 273.15) × 9/5 + 32 = -459.7°F
         return tempF;
     }
 
-    public void FarenheitVersKelvin(float tempF) {
+    public float FarenheitVersKelvin(float tempF) {
         float tempK;
-        tempK = (tempF - 32) * (5 / 9) + 273.15; // (32°F − 32) × 5/9 + 273.15 = 273.15K
+        tempK = (float) ((tempF - 32) * (5 / 9) + 273.15); // (32°F − 32) × 5/9 + 273.15 = 273.15K
         return tempK;
     }
 }
