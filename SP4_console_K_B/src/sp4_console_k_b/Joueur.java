79781acf08efc3cbf8c5058257768877487272dd
mario <mario@DESKTOP-NOK73NM>
2021-11-12 17:36:10 +0100

    reinitialisation de la classe Joueur

---
+++ SP4_console_K_B/src/sp4_console_k_b/Joueur.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sp4_console_k_b;
+
+/**
+ *
+ * @author mario
+ */
+public class Joueur {
+    
+    String Nom;
+    String Couleur;
+    int Liste_Jetons[ ] = new int[21];
+    int nombreDesintegrateurs;
+    int nombreJetonsRestants;
+    Jeton piece;
+    
+    
+    public Joueur(String name ){
+        name=Nom;
+    }
+    public void affecterCouleur(String color){
+        color=Couleur;
+    }
+    public boolean ajouterJeton(Jeton){
+        
+    }   
+    public void obtenirDesintegrateur(){
+        
+    }
+    public boolean utiliserDesintegrateur(){
+        
+    }
+}
