1a74f5fe3cd84f364ab4371c6a98ddec29f3b6c0
mario <mario@DESKTOP-NOK73NM>
2021-11-12 18:22:23 +0100

    amélioration classe Joueur

--- SP4_console_K_B/src/sp4_console_k_b/Cellule.java
+++ SP4_console_K_B/src/sp4_console_k_b/Cellule.java
@@ -4,11 +4,47 @@ package sp4_console_k_b;
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 /**
  *
  * @author leaburriat
  */
 public class Cellule {
-   
+
+    Jeton jetonCourant;
+    boolean trouNoir;
+    boolean desintegrateur;
+
+    public Cellule() {
+
+    }
+    
+    public Jeton recupererJeton() {
+
+    }
+
+    public boolean supprimerJeton() {
+
+    }
+
+    public boolean placerTrouNoir() {
+
+    }
+    public boolean placerDesintegrateur(){
+        
+    }
+    public boolean presenceTrouNoir(){
+        
+    }
+    public String lireCouleurDuJeton(){
+        
+    }
+    public boolean recupererDesintegrateur(){
+        
+    }
+    public boolean activerTrouNoir(){
+        
+    }
+    public boolean affecterJeton(Jeton) {
+
+    }
 }
