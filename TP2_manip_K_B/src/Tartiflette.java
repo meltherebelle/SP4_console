72b940bbab2fb38ab5b2720801258f5332b362f8
leaburriat <leaburriat@localhost>
2021-11-12 15:00:58 +0100

    finalisation Manip

---
+++ TP2_manip_K_B/src/Tartiflette.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author leaburriat
+ */
+package tp2_manip_k_b;
+ 
+public class Tartiflette {
+    int nbCalories;
+    
+    public Tartiflette(int nbCal) {
+        nbCalories = nbCal;
+    }
+}
