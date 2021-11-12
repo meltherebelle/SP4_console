ed39ca8bcc645a474f3d1a19882eda343af20184
leaburriat <leaburriat@localhost>
2021-11-12 18:14:03 +0100

    modif class Partie

--- SP4_console_K_B/src/sp4_console_k_b/Partie.java
+++ SP4_console_K_B/src/sp4_console_k_b/Partie.java
@@ -1,4 +1,5 @@
 package sp4_console_k_b;
+package Joueur.java;
 
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
@@ -12,17 +13,32 @@ package sp4_console_k_b;
 public class Partie {
     
     /// Attributs
-    
     //ListeJoueurs : tableau des deux joueurs de la partie
-    String Joueur[] = new String[2] ;
-    
+    Joueur [] ListeJoueurs = new Joueur[2] ;
     
     //joueurCourant : désigne le joueur courant à tout moment de la partie
-    
+    Joueur joueurCourant = new Joueur;
     
     //grilleJeu : désigne la grille de jeu. C’est sans doute l’objet le plus important de ce projet.
     //Tout y est fait : ajout de jetons, et vérification de la grille
+    Grille grilleJeu = new Grille;
     
     
+    //Constructeur de Partie
+    Partie(Joueur,Joueur) {
+        
+        public void initialiserPartie() {
+            
+        }
+        
+        public void debuterPartie() {
+            
+        }
+        
+        public void attribuerCouleursAuxJoueurs() {
+            
+        }
+        
+    }
     
 }
